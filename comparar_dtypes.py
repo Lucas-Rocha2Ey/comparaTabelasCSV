ac3df261c2a2bca5a8a7ad8ee0b0df3319feb0f6
Lucas Rocha <Lucas.Rocha2@br.ey.com>
2022-08-18 17:20:38 -0300

    adicionado teste com format=word*

--- comparar_dtypes.py
+++ comparar_dtypes.py
@@ -9,9 +9,16 @@ def testar_tipos(df_dtypes_databricks, df_dtypes_sas):
     status_approved = True  # Flag que irá mudar no final do teste para ver se o teste foi aprovado ou não.
 
     for cada_variavel in df_dtypes_sas['Variable'].values:
-        tipo_variavel_sas = df_dtypes_sas.query(f"Variable == '{cada_variavel}'")['Type'].values[0]
-        type_length = int(df_dtypes_sas.query(f"Variable == '{cada_variavel}'")['Len'].values[0])
-        tipo_variavel_databricks = df_dtypes_databricks.query(f"columns == '{cada_variavel}'")['dtype'].values[0]
+        try:
+            tipo_variavel_sas = df_dtypes_sas.query(f"Variable == '{cada_variavel}'")['Type'].values[0]
+            type_length = int(df_dtypes_sas.query(f"Variable == '{cada_variavel}'")['Len'].values[0])
+            tipo_variavel_databricks = df_dtypes_databricks.query(f"columns == '{cada_variavel}'")['dtype'].values[0]
+        except ValueError as value_error:
+            status_approved = False
+            if value_error.args[0] == 'cannot convert float NaN to integer':
+                print("Existe pelo menos um campo da coluna 'Len' vazia na tabela proc_contents no SAS.\n"
+                      "Abortando teste...")
+                break
 
         if 'char' in tipo_variavel_sas.lower():
             if type_length == 1:
@@ -104,6 +111,17 @@ def testar_tipos(df_dtypes_databricks, df_dtypes_sas):
                             f"Variável {cada_variavel}\nTipo SAS:{tipo_variavel_sas}\nTamanho do campo:{type_length}\n"
                             f"FORMAT: {type_format}\nTipo Databricks: {tipo_variavel_databricks}\n"
                             f"Resultado Esperado: date ou datetime\n")
+                elif 'word' in type_format.lower():
+                    if 'string' in tipo_variavel_databricks.lower():
+                        print(
+                            f"Variável {cada_variavel}\nTipo SAS:{tipo_variavel_sas}\nTamanho do campo:{type_length}\n"
+                            f"FORMAT: {type_format}\nTipo Databricks: {tipo_variavel_databricks}\nResultado: OK\n")
+                    else:
+                        status_approved = False
+                        print(
+                            f"Variável {cada_variavel}\nTipo SAS:{tipo_variavel_sas}\nTamanho do campo:{type_length}\n"
+                            f"FORMAT: {type_format}\nTipo Databricks: {tipo_variavel_databricks}\n"
+                            f"Resultado Esperado: string\n")
                 else:
                     # caso seja NUM, tamanho do campo > 1 e não tiver estabelecido um formato específico -> float,double
                     if ('float' in tipo_variavel_databricks.lower()) or ('double' in tipo_variavel_databricks.lower()):
